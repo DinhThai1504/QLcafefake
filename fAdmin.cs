7f436230260ea2ebad97e416a63248abf69b8062
KhoaKoha <kohavn90@gmail.com>
2025-02-18 12:00:58 +0700

    test

--- fAdmin.cs
+++ fAdmin.cs
@@ -15,6 +15,10 @@ namespace QLQuanCafe
 {
     public partial class fAdmin : Form
     {
+
+        BindingSource foodList = new BindingSource();
+
+        BindingSource accountList = new BindingSource();
         public fAdmin()
         {
             InitializeComponent();
@@ -31,11 +35,23 @@ namespace QLQuanCafe
         }
             void Load()
         {
+
+            // cua bai 21 
+              dtgvFood.DataSource = foodList;
+            dtgvAccount.DataSource = accountList;
+            // ket thuc
             LoadDateTimePickerBill();
             //LoadListBillByDate(dtpkFromDate.Value, dtpkToDate.Value);
+              //LoadListBillByDate(dtpkFromDate.Value, dtpkToDate.Value);
+             LoadAccount();
             LoadListFood();
             LoadCategoryIntoCombobox(cbFoodCategory);
             AddFoodBinding();
+        }
+
+         void LoadAccount()
+        {
+            accountList.DataSource = AccountDAO.Instance.GetListAccount();
         }
         void LoadDateTimePickerBill()
         {
@@ -48,8 +64,138 @@ namespace QLQuanCafe
             dtgvBill.DataSource = BillDAO.Instance.GetBillListByDate(checkIn, checkOut);
         }*/
 
+          //void LoadDateTimePickerBill()
+        //{
+        //    DateTime today = DateTime.Now;
+        //    dtpkFromDate.Value = new DateTime(today.Year, today.Month, 1);
+        //    dtpkToDate.Value = dtpkFromDate.Value.AddMonths(1).AddDays(-1);
+        //}
+        //void LoadListBillByDate(DateTime checkIn, DateTime checkOut)
+        //{
+        //    dtgvBill.DataSource = BillDAO.Instance.GetBillListByDate(checkIn, checkOut);
+        //}
 
-        void AddFoodBinding()
+        //void AddFoodBinding()
+        //{
+        //    txbFoodName.DataBindings.Add(new Binding("Text", dtgvFood.DataSource, "Name", true, DataSourceUpdateMode.Never));
+        //    txbFoodID.DataBindings.Add(new Binding("Text", dtgvFood.DataSource, "ID", true, DataSourceUpdateMode.Never));
+        //    nmFoodPrice.DataBindings.Add(new Binding("Value", dtgvFood.DataSource, "Price", true, DataSourceUpdateMode.Never));
+        //}
+
+        //void LoadCategoryIntoCombobox(ComboBox cb)
+        //{
+        //    cb.DataSource = CategoryDAO.Instance.GetListCategory();
+        //    cb.DisplayMember = "Name";
+        //}
+
+          void AddAccount(string userName, string displayName, int type)
+        {
+            if (AccountDAO.Instance.InsertAccount(userName, displayName, type))
+            {
+                MessageBox.Show("Thêm tài khoản thành công");
+            }
+            else
+            {
+                MessageBox.Show("Thêm tài khoản thất bại");
+            }
+
+            LoadAccount();
+        }
+
+
+  void EditAccount(string userName, string displayName, int type)
+        {
+            if (AccountDAO.Instance.UpdateAccount(userName, displayName, type))
+            {
+                MessageBox.Show("Cập nhật tài khoản thành công");
+            }
+            else
+            {
+                MessageBox.Show("Cập nhật tài khoản thất bại");
+            }
+
+            LoadAccount();
+        }
+
+         void DeleteAccount(string userName)
+        {
+            if (loginAccount.UserName.Equals(userName))
+            {
+                MessageBox.Show("Vui lòng đừng xóa chính bạn chứ");
+                return;
+            }
+            if (AccountDAO.Instance.DeleteAccount(userName))
+            {
+                MessageBox.Show("Xóa tài khoản thành công");
+            }
+            else
+            {
+                MessageBox.Show("Xóa tài khoản thất bại");
+            }
+
+            LoadAccount();
+        }
+
+
+  void ResetPass(string userName)
+        {
+            if (AccountDAO.Instance.ResetPassword(userName))
+            {
+                MessageBox.Show("Đặt lại mật khẩu thành công");
+            }
+            else
+            {
+                MessageBox.Show("Đặt lại mật khẩu thất bại");
+            }
+        }
+
+
+   private void btnAddAccount_Click(object sender, EventArgs e)
+        {
+            string userName = txbUserName.Text;
+            string displayName = txbDisPlayname.Text;
+            int type = (int)numericUpDown1.Value;
+
+            AddAccount(userName, displayName, type);
+        }
+
+
+  private void btnDeleteAccount_Click(object sender, EventArgs e)
+        {
+            string userName = txbUserName.Text;
+
+            DeleteAccount(userName);
+        }
+       void AddAccountBinding()
+        {
+            txbUserName.DataBindings.Add(new Binding("Text", dtgvAccount.DataSource, "UserName", true, DataSourceUpdateMode.Never));
+            txbDisPlayname.DataBindings.Add(new Binding("Text", dtgvAccount.DataSource, "DisplayName", true, DataSourceUpdateMode.Never));
+            numericUpDown1.DataBindings.Add(new Binding("Value", dtgvAccount.DataSource, "Type", true, DataSourceUpdateMode.Never));
+        }
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+            // Bạn có thể để trống hoặc thêm code tùy ý.
+        }
+
+
+          private void btnEditAccount_Click(object sender, EventArgs e)
+        {
+            string userName = txbUserName.Text;
+            string displayName = txbDisPlayname.Text;
+            int type = (int)numericUpDown1.Value;
+
+            EditAccount(userName, displayName, type);
+        }
+
+
+  private void btnDlmk_Click(object sender, EventArgs e)
+        {
+            string userName = txbUserName.Text;
+
+            ResetPass(userName);
+        }
+        
+                void AddFoodBinding()
         {
             txbFoodName.DataBindings.Add(new Binding("Text", dtgvFood.DataSource, "Name"));
             txbFoodID.DataBindings.Add(new Binding("Text", dtgvFood.DataSource, "ID"));
